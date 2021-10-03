fa267bbe0482a90b086ef94c84c2cae8bd901184
cazaresmr <michaelcazares@gmail.com>
2021-10-02 17:42:35 -0700

    corrected routes

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -21,26 +21,26 @@ partnerRouter.route("/")
     res.end('Deleting all partners');
 });
 
-partnerRouter.route('/partners/:partnerId')
+partnerRouter.route('/:partnerId')
 .all((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
 .get((req, res) => {
-    res.end(`Will send details fo the partners: ${req.params.campsiteId} to you`);
+    res.end(`Will send details fo the partners: ${req.params.partnerId} to you`)
 })
 .post((req, res) => {
     res.statusCode = 403;
-    res.end(`POST operation not supported on /partners/${req.params.campsiteId}`);
+    res.end(`POST operation not supported on /partners/${req.params.partnerId}`)
 })
 .put((req, res) => {
-    res.write(`Updating the partner: ${req.params.campsiteId}\n`);
+    res.write(`Updating the partner: ${req.params.partnerId}\n`)
     res.end(`Will update the partner ${req.body.name} 
         with description: ${req.body.description}`);
 })
 .delete((req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+    res.end(`Deleting campsite: ${req.params.partnerId}`)
 });
 
 module.exports = partnerRouter;
